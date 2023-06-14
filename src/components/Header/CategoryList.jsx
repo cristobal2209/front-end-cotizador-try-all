e1da138091ff2e754eac4bb06f733eee3a038971
Cristobal Caceres <cristobal.caceres.c@mail.pucv.cl>
2023-06-14 11:36:42 -0400

    cambios menores a CategoryList

--- src/components/Header/CategoryList.jsx
+++ src/components/Header/CategoryList.jsx
@@ -41,21 +41,20 @@ export default function CategoryList() {
   return (
     <div className="relative">
       <button
-        className="flex items-center rounded-md bg-transparent px-4 py-2 font-semibold text-white hover:bg-primaryHover hover:shadow-md"
+        className="flex items-center rounded-md bg-transparent px-4 mx-2 py-2 font-semibold text-white hover:bg-primaryHover hover:shadow-md"
         onClick={handleToggle}
       >
         Categorías
         <div className="pl-1">
           <ChevronDownIcon
             strokeWidth={2}
-            className={`block h-3 w-3 transition-transform ${
-              isOpen || isMobileCategoriesOpen ? "rotate-180" : ""
-            }`}
+            className={`block h-3 w-3 transition-transform ${isOpen || isMobileCategoriesOpen ? "rotate-180" : ""
+              }`}
           />
         </div>
       </button>
       {isOpen && (
-        <div className="fixed left-0 top-20 flex h-1/6 w-full items-center justify-center rounded-md bg-primary shadow-lg ">
+        <div className="fixed left-0 top-20 flex min-h-[220px] w-full items-center justify-center rounded-md bg-primary shadow-lg ">
           <div className="grid w-2/3 grid-cols-4 gap-2 px-5 duration-300 ease-in-out">
             {renderCategories}
           </div>
