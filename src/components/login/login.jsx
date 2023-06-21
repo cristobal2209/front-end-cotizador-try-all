58435c40ac016c68e7297cf43cf3a0a61117f021
Cristobal Caceres <cristobal.caceres.c@mail.pucv.cl>
2023-06-21 18:15:47 -0400

    a

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,28 +1,28 @@
 export default function Login() {
   return (
-    <main class=" bg-bgDark w-full">
-      <div class="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
+    <main className=" w-full bg-bgDark">
+      <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
         <a
           href="https://www.tryall.cl/"
-          class="mb-6 flex items-center text-2xl font-semibold text-quaternary dark:text-white"
+          className="mb-6 flex items-center text-2xl font-semibold text-quaternary dark:text-white"
         >
           <img
-            class="mr-2 h-15 w-40"
+            className="h-15 mr-2 w-40"
             src="src\assets\logo-try-all.png"
             alt="logo"
             href=""
           />
           Cotizaciones
         </a>
-        <div class="w-full sm:max-w-md md:mt-0 xl:p-0">
-          <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
-            <form class="space-y-4 md:space-y-6" action="#">
+        <div className="w-full sm:max-w-md md:mt-0 xl:p-0">
+          <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
+            <form className="space-y-4 md:space-y-6" action="#">
               <div>
                 <input
                   type="email"
                   name="email"
                   id="email"
-                  class="focus:ring-primary text-center text-white focus:border-primary-600 block w-full bg-primary rounded-lg border border-primary p-2.5 text-gray dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+                  className="focus:border-primary-600 text-gray block w-full rounded-lg border border-primary bg-primary p-2.5 text-center text-white focus:ring-primary dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
                   placeholder="Nombre de usuario"
                   required=""
                 />
@@ -33,32 +33,31 @@ export default function Login() {
                   name="password"
                   id="password"
                   placeholder="Contraseña"
-                  class="text-center text-white bg-primary focus:border-primary block w-full rounded-lg p-2.5 text-gray dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+                  className="text-gray block w-full rounded-lg bg-primary p-2.5 text-center text-white focus:border-primary dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
                   required=""
                 />
               </div>
-              <div class="flex items-center justify-between">
-                <div class="flex items-start">  
-              <button
-                type="Ingresar"
-                class="bg-secondary hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
-              >
-                Ingresar
-              </button>
-              
-              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <button
+                    type="Ingresar"
+                    className="hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg bg-secondary px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+                  >
+                    Ingresar
+                  </button>
+                </div>
                 <a
                   href="#"
-                  class="text-white dark:text-primary-500 text-sm font-medium hover:underline"
+                  className="dark:text-primary-500 text-sm font-medium text-white hover:underline"
                 >
                   ¿Olvidó su contraseña?
                 </a>
               </div>
-              <p class="text-sm text-center font-light text-white dark:text-gray-400">
+              <p className="text-sm text-center font-light text-white dark:text-gray-400">
                 ¿No posee cuenta?{" "}
                 <a
                   href="#"
-                  class="text-primary-600 dark:text-primary-500 font-medium hover:underline"
+                  className="text-primary-600 dark:text-primary-500 font-medium hover:underline"
                 >
                   Contáctenos
                 </a>
