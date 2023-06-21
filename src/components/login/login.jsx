c4343f7dc58b775d9768c89334a95ecbe7bcbb2c
Brandon Baxter <brandon.baxter.q@mail.pucv.cl>
2023-06-21 18:06:12 -0400

    feature-login-finish

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,96 +1,66 @@
 export default function Login() {
   return (
-    <main class="bg-gray-50 dark:bg-gray-900">
+    <main class=" bg-bgDark w-full">
       <div class="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
         <a
-          href="#"
-          class="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white"
+          href="https://www.tryall.cl/"
+          class="mb-6 flex items-center text-2xl font-semibold text-quaternary dark:text-white"
         >
           <img
-            class="mr-2 h-8 w-8"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+            class="mr-2 h-15 w-40"
+            src="src\assets\logo-try-all.png"
             alt="logo"
+            href=""
           />
-          Flowbite
+          Cotizaciones
         </a>
-        <div class="w-full rounded-lg bg-white shadow dark:border dark:border-gray-700 dark:bg-gray-800 sm:max-w-md md:mt-0 xl:p-0">
+        <div class="w-full sm:max-w-md md:mt-0 xl:p-0">
           <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
-              Sign in to your account
-            </h1>
             <form class="space-y-4 md:space-y-6" action="#">
               <div>
-                <label
-                  for="email"
-                  class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Your email
-                </label>
                 <input
                   type="email"
                   name="email"
                   id="email"
-                  class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
-                  placeholder="name@company.com"
+                  class="focus:ring-primary text-center text-white focus:border-primary-600 block w-full bg-primary rounded-lg border border-primary p-2.5 text-gray dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+                  placeholder="Nombre de usuario"
                   required=""
                 />
               </div>
               <div>
-                <label
-                  for="password"
-                  class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
                 <input
                   type="password"
                   name="password"
                   id="password"
-                  placeholder="••••••••"
-                  class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+                  placeholder="Contraseña"
+                  class="text-center text-white bg-primary focus:border-primary block w-full rounded-lg p-2.5 text-gray dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
                   required=""
                 />
               </div>
               <div class="flex items-center justify-between">
-                <div class="flex items-start">
-                  <div class="flex h-5 items-center">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      class="focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 h-4 w-4 rounded border border-gray-300 bg-gray-50 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800"
-                      required=""
-                    />
-                  </div>
-                  <div class="ml-3 text-sm">
-                    <label
-                      for="remember"
-                      class="text-gray-500 dark:text-gray-300"
-                    >
-                      Remember me
-                    </label>
-                  </div>
-                </div>
+                <div class="flex items-start">  
+              <button
+                type="Ingresar"
+                class="bg-secondary hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+              >
+                Ingresar
+              </button>
+              
+              </div>
                 <a
                   href="#"
-                  class="text-primary-600 dark:text-primary-500 text-sm font-medium hover:underline"
+                  class="text-white dark:text-primary-500 text-sm font-medium hover:underline"
                 >
-                  Forgot password?
+                  ¿Olvidó su contraseña?
                 </a>
               </div>
-              <button
-                type="submit"
-                class="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
-              >
-                Sign in
-              </button>
-              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don’t have an account yet?{" "}
+              <p class="text-sm text-center font-light text-white dark:text-gray-400">
+                ¿No posee cuenta?{" "}
                 <a
                   href="#"
                   class="text-primary-600 dark:text-primary-500 font-medium hover:underline"
                 >
-                  Sign up
+                  Contáctenos
                 </a>
               </p>
             </form>
