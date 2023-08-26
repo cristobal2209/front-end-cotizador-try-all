f0a2d73017804e8f780be26cc7b5cb60d61b0c78
Cristobal Caceres <cristobal.caceres.c@mail.pucv.cl>
2023-08-26 13:26:15 -0400

    se finaliza componente create quote

--- src/components/Header/CreateQuote.jsx
+++ src/components/Header/CreateQuote.jsx
@@ -10,6 +10,7 @@ import { createQuote } from "../../services/createQuoteService";
 import {
   CheckIcon,
   ExclamationTriangleIcon,
+  XMarkIcon,
 } from "@heroicons/react/24/outline";
 
 import {
@@ -32,8 +33,13 @@ const TEMPLATE_QUOTE = {
 
 const validationSchema = Yup.object().shape({
   quoteName: Yup.string()
-    .min(3, "El nombre debe tener al menos 3 caracteres")
-    .required("El nombre es obligatorio"),
+    .min(3, "El nombre debe contener al menos 3 caracteres")
+    .max(20, "El nombre debe contener a lo mas 20 caracteres")
+    .required("El nombre es obligatorio")
+    .matches(
+      /^[a-zA-Z0-9]*$/,
+      "No se permiten caracteres especiales en el nombre"
+    ),
 });
 
 export default function CreateQuote({ UUID }) {
@@ -56,7 +62,8 @@ export default function CreateQuote({ UUID }) {
     },
     validationSchema: validationSchema,
     onSubmit: () => {
-      handleConfirmName();
+      quoteData.quoteName = formik.values.quoteName;
+      handleOpenQuoteNameAlert();
     },
   });
 
@@ -78,7 +85,6 @@ export default function CreateQuote({ UUID }) {
   };
 
   const handleSetInitialQuoteData = () => {
-    quoteData.quoteName = formik.values.quoteName;
     quoteData.date = currentDate.toDateString();
     quoteData.version = "1.0";
     quoteData.state = 1;
@@ -92,16 +98,12 @@ export default function CreateQuote({ UUID }) {
     setOpenQuoteNameAlert(!openQuoteNameAlert);
   };
 
-  const handleConfirmName = () => {
-    handleSetInitialQuoteData();
-    handleOpenQuoteNameAlert();
-  };
-
   return (
     <>
       {isEditing ? (
         <>
           <Input
+            variant="standard"
             name="quoteName"
             label="Nombre cotización"
             color="white"
@@ -112,7 +114,7 @@ export default function CreateQuote({ UUID }) {
             required
           />
           {formik.touched.quoteName && formik.errors.quoteName ? (
-            <Alert className="absolute mt-[110px] bg-red-500 !w-auto animate-pulse">
+            <Alert className="absolute mt-[130px] bg-red-500 !w-auto animate-pulse">
               <div className="flex flex-row items-center">
                 <ExclamationTriangleIcon className="mr-1 h-4 w-4" />
                 {formik.errors.quoteName}
@@ -121,20 +123,27 @@ export default function CreateQuote({ UUID }) {
           ) : null}
           <Button
             size="sm"
-            className="mx-1 rounded bg-quaternary shadow-none hover:bg-quaternaryHover hover:shadow-none"
+            className="mx-1 rounded bg-quaternary shadow-none hover:bg-quaternaryHover !px-2"
             onClick={() => formik.handleSubmit()}
           >
-            <CheckIcon className="h-4 w-4" />
+            <CheckIcon className="h-3 w-3" />
+          </Button>
+          <Button
+            size="sm"
+            className="mx-1 rounded bg-red-600 shadow-none hover:bg-red-400 !px-2"
+            onClick={() => handleEditing()}
+          >
+            <XMarkIcon className="h-3 w-3" />
           </Button>
         </>
       ) : (
         <>
           <Button
             size="sm"
-            className="mx-1 rounded bg-quaternary hover:bg-quaternaryHover"
+            className="mx-1 rounded bg-quaternary hover:bg-quaternaryHover font-light"
             onClick={handleEditing}
           >
-            Nueva cotizacion
+            Nueva cotización
           </Button>
         </>
       )}
