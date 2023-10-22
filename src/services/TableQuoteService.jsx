87c244d0b20d90fe5f07b16ca81363ba1b133141
Cristobal Caceres <cristobal.caceres.c@mail.pucv.cl>
2023-10-22 20:16:09 -0300

    se permite establecer el estado de una cotizacion

--- src/services/TableQuoteService.jsx
+++ src/services/TableQuoteService.jsx
@@ -1,5 +1,13 @@
 import { auth, db } from "../firebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  query,
+  where,
+} from "firebase/firestore";
 
 //se busca que documento tiene el atributo "initialized" y se quita del arreglo
 const filterUserQuotes = (userQuotes) => {
@@ -31,4 +39,45 @@ export const getUserQuotes = async () => {
 
   //se saca el documento de cotizacion de inicializacion
   return filterUserQuotes(userQuotes);
+};
+
+export const changeQuoteStatus = async (quoteUID, newStatus) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const user = auth.currentUser;
+      if (!user) {
+        reject("Usuario no autenticado");
+        return;
+      }
+
+      const quoteRef = doc(db, "usersQuotes", user.uid, "quotes", quoteUID);
+      await updateDoc(quoteRef, { state: newStatus });
+
+      resolve(newStatus);
+    } catch (error) {
+      console.error("Error al actualizar el estado:", error);
+      reject(error);
+    }
+  });
+};
+
+export const checkActiveQuotesExists = async () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const user = auth.currentUser;
+      if (!user) {
+        reject("Usuario no autenticado");
+        return;
+      }
+
+      const quotesRef = collection(db, "usersQuotes", user.uid, "quotes");
+      const q = query(quotesRef, where("state", "==", 1));
+      const querySnapshot = await getDocs(q);
+
+      resolve(querySnapshot.empty);
+    } catch (error) {
+      console.error("Error al actualizar el estado:", error);
+      reject(error);
+    }
+  });
 };
