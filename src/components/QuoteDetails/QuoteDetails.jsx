084025b6b60ef0e576644da6d3506402aabf6091
Cristobal Caceres <cristobal.caceres.c@mail.pucv.cl>
2023-11-02 22:58:18 -0300

    se actualiza componente de cotizaciones. Ahora se permite ver el precio por la cantidad del producto y se actualiza automaticamente el precio de acuerdo a eso

--- src/components/QuoteDetails/QuoteDetails.jsx
+++ src/components/QuoteDetails/QuoteDetails.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
 import {
   Card,
   CardBody,
@@ -7,10 +9,10 @@ import {
   Button,
   Typography,
   Spinner,
+  Tooltip,
 } from "@material-tailwind/react";
 import { useParams } from "react-router-dom";
 import {
-  getQuote,
   updateQuoteProducts,
   subscribeToActiveQuote,
 } from "../../services/QuoteService";
@@ -40,6 +42,7 @@ export default function QuoteDetails() {
 
   useEffect(() => {
     if (quote?.products.length != 0) setQuoteProducts(quote?.products);
+    //console.log(quote);
   }, [quote]);
 
   useEffect(() => {
@@ -48,13 +51,14 @@ export default function QuoteDetails() {
       updateProducts();
       setCounter(counter + 1);
     }
-    console.log(quoteProducts);
   }, [quoteProducts]);
 
   const deleteProduct = async (index) => {
-    let copyQuoteProducts = quoteProducts;
-    copyQuoteProducts.splice(index, 1);
-    setQuoteProducts(copyQuoteProducts);
+    setQuoteProducts((prevProducts) => {
+      const newProducts = [...prevProducts];
+      newProducts.splice(index, 1);
+      return newProducts;
+    });
     updateProducts();
     setCounter(counter + 1);
   };
@@ -80,10 +84,21 @@ export default function QuoteDetails() {
     let total = 0;
     quoteProducts?.forEach((productData) => {
       const quantity = productData.quantity;
-      let price = parseFloat(
-        productData.supplier.prices[0].price.replace(/\$/g, "")
-      );
-      total += price * quantity;
+      const priceMap = {}; // Objeto para mapear cantidades a precios
+
+      //Crear el mapeo de cantidades a precios
+      productData.supplier.prices.forEach((priceEntry) => {
+        const minQuantity = parseInt(priceEntry.quantity.replace("+", ""));
+        const price = parseFloat(priceEntry.price.replace(/\$/g, ""));
+        priceMap[minQuantity] = price;
+      });
+      let nearestQuantity = quantity;
+      while (priceMap[nearestQuantity] === undefined && nearestQuantity >= 0) {
+        nearestQuantity--;
+      }
+
+      const newPrice = priceMap[nearestQuantity] || 0;
+      total += newPrice * quantity;
     });
     setTotal(total.toFixed(2));
   };
@@ -157,7 +172,7 @@ export default function QuoteDetails() {
         </CardBody>
         <CardFooter className="flex items-center justify-between p-4">
           <Typography variant="h5" className="text-light">
-            Total: {total}
+            Total: ${total}
           </Typography>
         </CardFooter>
       </Card>
@@ -175,52 +190,84 @@ function ProductQuoteRow({
   const [priceNumber, setPriceNumber] = useState(0);
   const [quantity, setQuantity] = useState(0);
   const [subtotal, setSubtotal] = useState(0);
+  const [totalStock, setTotalStock] = useState(0);
+  const { product, supplier } = productData;
+
+  const priceMap = {}; // Objeto para mapear cantidades a precios
+
+  // Crear el mapeo de cantidades a precios
+  supplier.prices.forEach((priceEntry) => {
+    const minQuantity = parseInt(priceEntry.quantity.replace("+", ""));
+    const price = parseFloat(priceEntry.price.replace(/\$/g, ""));
+    priceMap[minQuantity] = price;
+  });
 
   useEffect(() => {
-    let price = parseFloat(
-      productData.supplier.prices[0].price.replace(/\$/g, "")
-    );
-    setPriceNumber(price);
     setQuantity(productData.quantity);
-    setSubtotal(priceNumber * quantity);
+    updatePriceAndSubtotal(productData.quantity);
+  }, [productData]);
+
+  useEffect(() => {
+    sumTotalStock();
   }, []);
 
-  useEffect(() => {
-    setSubtotal(priceNumber * quantity);
-  }, [quantity]);
+  const updatePriceAndSubtotal = (newQuantity) => {
+    // Buscar el precio más cercano en el mapeo
+    let nearestQuantity = newQuantity;
+    while (priceMap[nearestQuantity] === undefined && nearestQuantity >= 0) {
+      nearestQuantity--;
+    }
+
+    const newPrice = priceMap[nearestQuantity] || 0;
+    setQuantity(newQuantity);
+    setPriceNumber(newPrice);
+    setSubtotal(newPrice * newQuantity);
+  };
+
+  const sumTotalStock = () => {
+    const stockArr = supplier.stock;
+    let totalStock = 0;
+
+    stockArr.forEach((stockObj) => {
+      totalStock += parseInt(stockObj.stock);
+    });
+
+    setTotalStock(totalStock);
+  };
 
   const increaseQuantity = () => {
-    // Actualizar la cantidad en el componente principal
-    setQuantity(quantity + 1);
-    updateSubtotal(index, quantity + 1);
+    const newQuantity = quantity + 1;
+    updatePriceAndSubtotal(newQuantity);
+    updateSubtotal(index, newQuantity);
   };
 
   const decreaseQuantity = () => {
-    // Actualizar la cantidad en el componente principal
-    setQuantity(quantity - 1);
-    updateSubtotal(index, quantity - 1);
+    if (quantity > 1) {
+      const newQuantity = quantity - 1;
+      updatePriceAndSubtotal(newQuantity);
+      updateSubtotal(index, newQuantity);
+    }
   };
 
   return (
-    <tr className={`bg-two text-light items-center ${classes}`}>
+    <tr
+      className={`bg-two hover:bg-twoHover text-light items-center ${classes}`}
+    >
       <td>
         <div className="flex p-4">
           <Typography
             variant="small"
             className="font-normal text-light mr-1 max-w-xs"
           >
-            {productData.product.description}
+            {product.description}
           </Typography>
           <div className="ml-auto">
-            <img
-              src={productData.product.imgSrc}
-              className="ml-1 rounded-md"
-            ></img>
+            <img src={product.imgSrc} className="ml-1 rounded-md"></img>
           </div>
         </div>
       </td>
       <td>
-        <div className="p-4">{productData.supplier.supplier}</div>
+        <div className="p-4">{supplier.supplier}</div>
       </td>
       <td>
         <div className="flex p-4">
@@ -247,6 +294,7 @@ function ProductQuoteRow({
           <div className="self-center mx-5">{quantity}</div>
           <Button
             onClick={increaseQuantity}
+            disabled={quantity >= totalStock}
             className="bg-three hover:bg-threeHover px-3"
           >
             <svg
@@ -264,10 +312,49 @@ function ProductQuoteRow({
               />
             </svg>
           </Button>
+          <div className="ml-1 self-center">
+            <Tooltip
+              content={
+                <div className="w-52">
+                  <Typography variant="small">Precios por cantidad</Typography>
+                  {supplier.prices?.map((price, index) => {
+                    return (
+                      <div className="flex justify-between" key={index}>
+                        <Typography variant="small">
+                          <span className="opacity-70">Cantidad:</span>&nbsp;
+                          {price.quantity}
+                        </Typography>
+                        <Typography variant="small">
+                          <span className="opacity-70"> Precio:</span>
+                          &nbsp;{price.price}
+                        </Typography>
+                      </div>
+                    );
+                  })}
+                </div>
+              }
+              className="bg-four shadow-md"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+                className="h-5 w-5 cursor-pointer text-light"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                />
+              </svg>
+            </Tooltip>
+          </div>
         </div>
       </td>
       <td>
-        <div className="mx-5">{subtotal.toFixed(2)}</div>
+        <div className="mx-5">${subtotal.toFixed(2)}</div>
       </td>
       <td>
         <div className="p-4 flex">
@@ -298,3 +385,31 @@ function ProductQuoteRow({
     </tr>
   );
 }
+
+ProductQuoteRow.propTypes = {
+  productData: PropTypes.shape({
+    product: PropTypes.shape({
+      description: PropTypes.string.isRequired,
+      imgSrc: PropTypes.string.isRequired,
+    }).isRequired,
+    supplier: PropTypes.shape({
+      supplier: PropTypes.string.isRequired,
+      prices: PropTypes.arrayOf(
+        PropTypes.shape({
+          quantity: PropTypes.string.isRequired,
+          price: PropTypes.string.isRequired,
+        })
+      ).isRequired,
+      stock: PropTypes.arrayOf(
+        PropTypes.shape({
+          stock: PropTypes.string.isRequired,
+        })
+      ).isRequired,
+    }).isRequired,
+    quantity: PropTypes.number.isRequired,
+  }).isRequired,
+  classes: PropTypes.string.isRequired,
+  updateSubtotal: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+  deleteProduct: PropTypes.func.isRequired,
+};
