b4f6e4dacac364c34f9729a10195d53fc1e30bec
Cristobal Caceres <cristobal.caceres.c@mail.pucv.cl>
2023-11-09 21:41:19 -0300

    se agrega puerto

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    server: {
+      port: 4000, // Puedes cambiar el número de puerto según tus preferencias
+    },
     proxy: {
       //"/api": "http://162.212.155.221:5000",
       "/api": "http://localhost:5000",
