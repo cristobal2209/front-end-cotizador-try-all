e58c4fb65cdae359164d8ece73b731d9ffad2f92
Cristobal Caceres <cristobal.caceres.c@mail.pucv.cl>
2023-11-10 15:37:03 -0300

    a

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// export default defineConfig({
+//   server: {
+//     port: import.meta.env.VITE_APP_PORT,
+//     proxy: {
+//       "/api": `${import.meta.env.VITE_BACKEND_HOST}:${
+//         import.meta.env.VITE_BACKEND_PORT
+//       }`,
+//     },
+//   },
+//   plugins: [react()],
+// });
 export default defineConfig({
   server: {
-    server: {
-      port: 4000,
-    },
+    port: 4000,
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": `http://localhost:5000`,
     },
   },
   plugins: [react()],
