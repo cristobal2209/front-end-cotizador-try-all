522d01bf149bdb7f5be64e0fd00303ca3101b72c
Cristobal Caceres <cristobal.caceres.c@mail.pucv.cl>
2023-11-10 16:59:06 -0300

    se agrega host

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ import react from "@vitejs/plugin-react";
 // });
 export default defineConfig({
   server: {
+    host: "0.0.0.0",
     port: 4000,
     proxy: {
       "/api": `http://localhost:5000`,
