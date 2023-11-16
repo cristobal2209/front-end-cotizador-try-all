a30a76131fc1437cce8f02203c76d3c3a2f853dc
marce <marceloski64@gmail.com>
2023-11-16 17:32:17 -0300

    Cambios Cliente para conectarse a server-typesense prod

--- src/services/typesense.js
+++ src/services/typesense.js
@@ -1,13 +1,13 @@
 import Typesense from "typesense";
 
 export const client = new Typesense.Client({
-    nodes: [
-      {
-        host: "localhost", // For Typesense Cloud use xxx.a1.typesense.net
-        port: 8108, // For Typesense Cloud use 443
-        protocol: "http", // For Typesense Cloud use https
-      },
-    ],
-    apiKey: "xyz",
-    connectionTimeoutSeconds: 2,
-  });
+  nodes: [
+    {
+      host: "162.212.155.221", // For Typesense Cloud use xxx.a1.typesense.net
+      port: 8108, // For Typesense Cloud use 443
+      protocol: "http", 
+    },
+  ],
+  apiKey: "brandon",
+  connectionTimeoutSeconds: 2,
+});
