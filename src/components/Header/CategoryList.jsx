d12167f332e517bd87c6fed81f24cc314039a258
marce <marceloski64@gmail.com>
2023-11-20 11:18:12 -0300

    Arreglos category list

--- src/components/Header/CategoryList.jsx
+++ src/components/Header/CategoryList.jsx
@@ -1,6 +1,8 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebaseConfig";
 import { useEffect, useState } from "react";
-import { getProductsFromCategory } from "../../services/SearchService";
-import { useNavigate } from "react-router-dom";
+import { getProductsFromCategory } from "../../services/SearchService"; 
+import { useNavigate } from 'react-router-dom';
 export default function CategoryList() {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -9,15 +11,34 @@ export default function CategoryList() {
     const [searchResults, setSearchResults] = useState([]);
     getProductsFromCategory(categoryName);
     navigate(`/search/${encodeURIComponent(categoryName)}`);
+    
+    
   };
+  useEffect(() => {
+    async function fetchCategories() {
+      try {
+        const querySnapshot = await getDocs(collection(db, "categories"));
+        const categoriesData = querySnapshot.docs.map((doc) => doc.data());
+        setCategories(categoriesData);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+        setLoading(false);
+      }
+    }
+
+    fetchCategories();
+  }, []);
 
   return (
-    <div className=" flex h-full w-full items-center justify-center">
-      <div className="grid w-3/4  grid-cols-4 gap-2 ">
-        {newCategories.map((category, index) => {
-          return (
+    <div className="flex h-full w-full items-center justify-center">
+      {loading ? (
+        <p>Loading categories...</p>
+      ) : (
+        <div className="grid w-3/4 grid-cols-4 gap-2">
+          {categories.map((category, index) => (
             <a
-              href={`/search/${category.categoryName}`}
+              href={`/search/${(category.categoryName)}`}
               key={index}
               className="rounded-md text-light font-semibold hover:bg-transparent hover:shadow-lg"
               onClick={() => handleCategoryClick(category.categoryName)}
@@ -26,13 +47,12 @@ export default function CategoryList() {
                 <div className="rounded-lg bg-white p-3 shadow-md">
                   <img src={category.image} alt={category.categoryName} />
                 </div>
-                {/* titulo categoria */}
                 <div className="text-left">{category.categoryName}</div>
               </div>
             </a>
-          );
-        })}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
