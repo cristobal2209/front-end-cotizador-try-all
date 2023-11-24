1a1c468512378a6a217d13ccd4f10d3c0255376a
Cristobal Caceres <cristobal.caceres.c@mail.pucv.cl>
2023-11-23 21:33:39 -0300

    cambios de puertos

--- src/services/typesense.js
+++ src/services/typesense.js
@@ -4,9 +4,9 @@ export const client = new Typesense.Client({
   nodes: [
     {
       host: "quotemaster.homedns.org",
-      port: 8443,
+      port: 443,
       protocol: "https",
-     // path: "/typesense",
+      // path: "/typesense",
     },
   ],
   apiKey: "brandon",
